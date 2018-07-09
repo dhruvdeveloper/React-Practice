e438b9bd33e2667be7112966a5554f394baee6cf
Dhruv thakkar <thakkardhruv97@gmail.com>
2018-07-09 12:46:04 +0530

    this is commit for function component 2

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <h2>Name of Student</h2>
-          <Name name = "Yash" education = "M.S in Computer Science." color = "color-orange"/>
-          <Name name = "Dhruv" education = "B.E in Computer Engineering." color = "color-red"/>
-          <Name name = "vishal" education = "B.E in Computer Engineering." color = "color-red"/>
-          <Name name = "priyank" education = "B.E in Computer Engineering." color = "color-orange"/>
+          <h2>Name of Children in Family.</h2>
+          <Name name = "Sweety" education = "MSC in IT" color = "color-red"/>
+          <Name name = "Yash" education = "M.S in Computer Science." color = "color-blue"/>
+          <Name name = "Riddhi" education = "MBA in IT" color = "color-pink"/>
+          <Name name = "vishal" education = "B.E in Computer Engineering." color = "color-orange"/>
+          <Name name = "Dhruv" education = "B.E in Computer Engineering." color = "color-black"/>
+          <Name name = "Chinmay" education = "Future MBA" color = "color-gold"/>
+
+
       </div>
     );
   }
